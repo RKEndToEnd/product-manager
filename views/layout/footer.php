6c57c27cf06f27915e419733e450f8450bc00510
RK EndToEnd <rkendtoend@gmail.com>
2022-03-10 15:43:31 +0100

    footer position fixed

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,5 +1,6 @@
-            <footer class="pt-3 mt-4 text-muted text-center border-top">
+            <footer class="pt-3 mt-4 text-muted text-center border-top fixed-bottom">
                  &copy; www.rkendtoend.pl <?php echo date('Y'); ?>
+                <div class="mb-3"></div>
             </footer>
         </main>
         <script src="<?php echo main_url(); ?>resources/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
